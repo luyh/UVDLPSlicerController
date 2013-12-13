4d88bbe394b5acb66b41af511d02d03bbec608d9
shaise <shaise@gmail.com>
2013-12-13 13:49:47 +0200

    add missing files

---
+++ UVDLP/Software/PC/UV_DLP_3dPrinter/UV_DLP_3D_Printer/3DEngine/ModelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine3D
+{
+    public abstract class ModelLoader
+    {
+        String m_lastError = null;
+        protected String m_fileExt;
+        public abstract Object3d LoadModel(String filename);
+
+        protected void LogError(String err)
+        {
+            m_lastError = err;
+        }
+
+        String FileExt
+        {
+            get { return m_fileExt; }
+        }
+
+        String LastError
+        {
+            get { return m_lastError; }
+        }
+    }
+}
---
+++ UVDLP/Software/PC/UV_DLP_3dPrinter/UV_DLP_3D_Printer/3DEngine/ModelLoaderAmf.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Engine3D
+{
+    public class ModelLoaderAmf : ModelLoader
+    {
+        public enum eUnit
+        {
+            Millimeter = 0,
+            Inch
+        }
+        XmlDocument m_xdoc;
+        XmlAttribute m_verattr;
+        Object3d m_object;
+
+
+        public XmlNode m_toplevel;
+        public int m_version;
+        public ModelLoaderAmf()
+        {
+            m_fileExt = "amf";
+        }
+
+        public override Object3d LoadModel(string filename)
+        {
+            try
+            {
+                m_xdoc = new XmlDocument();
+                m_xdoc.Load(filename);
+                m_toplevel = m_xdoc.ChildNodes[1];
+                if (m_toplevel.Name != "amf")
+                {
+                    LogError(filename + " is not a valid AMF file");
+                    return null;
+                }
+                m_object = new Object3d();
+
+            }
+            catch (Exception ex)
+            {
+                LogError(ex.Message);
+            }
+            return null;
+        }
+
+
+        String GetAttr(XmlNode node, String attr, String defval)
+        {
+            try
+            {
+                String res = node.Attributes[attr].Value;
+                return res;
+            }
+            catch (Exception) { }
+            return defval;
+        }
+
+        bool ParseBody(XmlNode xBody)
+        {
+            return true;
+        }
+    }
+}
