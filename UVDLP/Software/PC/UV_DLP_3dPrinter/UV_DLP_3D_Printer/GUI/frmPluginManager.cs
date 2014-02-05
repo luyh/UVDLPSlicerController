dbad86e0604c73d84f96441af992df7a415964e1
Stephen Hernandez <pacmanfan321@gmail.com>
2014-02-04 22:38:14 -0500

    First working pass at licensing plugins

--- UVDLP/Software/PC/UV_DLP_3dPrinter/UV_DLP_3D_Printer/GUI/frmPluginManager.cs
+++ UVDLP/Software/PC/UV_DLP_3dPrinter/UV_DLP_3D_Printer/GUI/frmPluginManager.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using UV_DLP_3D_Printer.Plugin;
+using CreationWorkshop.Licensing;
+using UV_DLP_3D_Printer.Licensing;
 
 namespace UV_DLP_3D_Printer.GUI
 {
@@ -26,7 +28,6 @@ namespace UV_DLP_3D_Printer.GUI
             foreach (PluginEntry ip in UVDLPApp.Instance().m_plugins)
             {
                 ListViewItem lvi = new ListViewItem(ip.m_plugin.Name);
-                lvi.SubItems.Add(ip.m_enabled.ToString());
                 lvi.SubItems.Add(ip.m_licensed.ToString());
                 lvi.SubItems.Add(ip.m_plugin.GetString("Version"));
                 lvi.SubItems.Add(ip.m_plugin.GetString("Description"));
@@ -36,24 +37,7 @@ namespace UV_DLP_3D_Printer.GUI
 
         private void UpdateButtons() 
         {
-            if (ipsel == null)
-            {
-                cmdEnable.Enabled = false;
-                return;
-            }
-            else 
-            {
-                cmdEnable.Enabled = true;            
-            }
-
-            if (ipsel.m_enabled)
-            {
-                cmdEnable.Text = "Disable";
-            }
-            else 
-            {
-                cmdEnable.Text = "Enable";
-            }
+            if (ipsel == null) return;
             if (ipsel.m_licensed)
             {
                 cmdLicense.Enabled = false;
@@ -74,25 +58,45 @@ namespace UV_DLP_3D_Printer.GUI
             }
         }
 
-        private void cmdEnable_Click(object sender, EventArgs e)
-        {
-            if (cmdEnable.Text == "Disable")
-            {
-                ipsel.m_enabled = false;
-            }
-            else 
-            {
-                ipsel.m_enabled = true;
-            }
-            UpdateButtons();
-            SetupPlugins();
-            ipsel = null;
-            UpdateButtons();
-        }
 
         private void cmdLicense_Click(object sender, EventArgs e)
         {
-
+            // get the text string from 
+            string license = txtLicense.Text;
+            license = license.Trim();
+            if (license.Length != 32) 
+            {
+                MessageBox.Show("Invalid license, please re-check");
+                return;
+            }
+            LicenseKey lk = new LicenseKey();
+            try
+            {
+                lk.Init(license);
+                if (lk.valid)
+                {
+                    txtLicense.Text = "";
+                    //should really check, vendor id against plugin vendor id
+                    KeyRing.Instance().m_keys.Add(lk);
+                    string licensefile = UVDLPApp.Instance().m_apppath + UVDLPApp.m_pathsep + "licenses.key";
+                    if (KeyRing.Instance().Save(licensefile))
+                    {
+                        MessageBox.Show("License Added to Keyring, Restart to take effect");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error Saving Keyring");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid license, please re-check");
+                }
+            }
+            catch (Exception) 
+            {
+                MessageBox.Show("Error validating license, please re-check");
+            }
         }
     }
 }
